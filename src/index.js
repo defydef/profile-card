c0b085c59443adb8f6876cc681ef2d911b6c1d2a
defy <8841170+defydef@users.noreply.github.com>
2023-07-15 22:04:40 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img src="avatar.jpeg" alt="avatar profile" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <main>
+      <h1>Defy Defdef</h1>
+      <p>
+        I have always been passionate about building user-friendly and
+        interactive web apps. In college, I was a member of the web development
+        club and worked on several projects, including a student portal and a
+        campus news website. I'm also a self-taught designer and has a keen eye
+        for typography and color.
+      </p>
+    </main>
+  );
+}
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      <li>
+        <Skill name="HTML + CSS" bgColor="lightblue" emoji="&#128170;" />
+      </li>
+      <li>
+        <Skill name="Javascript" bgColor="yellow" emoji="&#127829;" />
+      </li>
+      <li>
+        <Skill name="Web Design" bgColor="red" emoji="&#127828;" />
+      </li>
+      <li>
+        <Skill name="Git and Github" bgColor="pink" emoji="&#127871;" />
+      </li>
+      <li>
+        <Skill name="React" bgColor="orange" emoji="&#127841;" />
+      </li>
+    </ul>
+  );
+}
+
+function Skill(props) {
+  return (
+    <p className="skill" style={{ backgroundColor: props.bgColor }}>
+      {props.name}
+      <span> {props.emoji}</span>
+    </p>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
